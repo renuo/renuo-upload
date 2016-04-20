d6994fafae92865119008acc92b407bfe4603ee1
Simon Huber <simon.huber@renuo.ch>
2016-04-20 17:11:02 +0200

    inline paths

--- gulpfile.js
+++ gulpfile.js
@@ -15,19 +15,6 @@ var rename = require('gulp-rename');
 var tsProject = ts.createProject('tsconfig.json');
 var tsSpecsProject = ts.createProject('tsconfig_specs.json');
 
-const paths = {
-  src: 'src/',
-  srcTsFiles: 'src/**/*.ts',
-  srcSpecFiles: 'src/**/*.spec.ts',
-  dist: 'dist/',
-  distFiles: 'dist/*',
-  tmp: '.tmp/',
-  tmpSpecFiles: ['.tmp/specs.js', '.tmp/specs.js.map'],
-  tmpHtmlFiles: '.tmp/*.html',
-  tmpFiles: '.tmp/*',
-  demoFiles: 'demo/**/*.html'
-};
-
 // TODO: setup protractor
 
 gulp.task('test', ['tscompile-specs'], function (done) {
@@ -43,29 +30,29 @@ gulp.task('test-raw', function (done) {
 });
 
 gulp.task('tslint', function () {
-  return gulp.src(paths.srcTsFiles)
+  return gulp.src('src/**/*.ts')
     .pipe(tslint())
     .pipe(tslint.report('prose'));
 });
 
 gulp.task('clean-all', function (callback) {
-  return del(paths.tmpFiles, callback);
-  return del(paths.distFiles, callback);
+  return del('.tmp/*', callback);
+  return del('dist/*', callback);
 });
 
 gulp.task('copyhtml', ['clean-html'], function () {
-  return gulp.src(paths.demoFiles)
-    .pipe(gulp.dest(paths.tmp))
+  return gulp.src('demo/**/*.html')
+    .pipe(gulp.dest('.tmp/'))
     .pipe(connect.reload());
 });
 
 gulp.task('clean-html', function (callback) {
-  return del(paths.tmpHtmlFiles, callback);
+  return del('.tmp/*.html', callback);
 });
 
 gulp.task('serve', ['copyhtml', 'tslint', 'tscompile', 'tscompile-specs'], function (callback) {
   connect.server({
-    root: [paths.tmp, './bower_components'],
+    root: ['.tmp/', './bower_components'],
     livereload: true,
     host: 'renuo-upload.dev'
   });
@@ -80,12 +67,12 @@ gulp.task('tdd', ['copyhtml', 'tslint', 'tscompile', 'tscompile-specs'], functio
 });
 
 gulp.task('ts-single-compile', ['clean-js-main'], function () {
-  return gulp.src(paths.src + 'renuo_upload.ts')
+  return gulp.src('src/' + 'renuo_upload.ts')
     .pipe(sourcemaps.init())
     .pipe(ts(tsProject))
     .pipe(babel({presets: ['es2015']}))
     .pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: 'src'}))
-    .pipe(gulp.dest(paths.tmp))
+    .pipe(gulp.dest('.tmp/'))
     .pipe(connect.reload());
 });
 
@@ -94,22 +81,22 @@ gulp.task('clean-js-main', function (callback) {
 });
 
 gulp.task('ts-specs-compile', ['clean-js-specs'], function () {
-  return gulp.src(paths.srcSpecFiles)
+  return gulp.src('src/**/*.spec.ts')
     .pipe(sourcemaps.init())
     .pipe(ts(tsSpecsProject))
     .pipe(babel({presets: ['es2015']}))
     .pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: 'src'}))
-    .pipe(gulp.dest(paths.tmp))
+    .pipe(gulp.dest('.tmp/'))
     .pipe(connect.reload());
 });
 
 gulp.task('clean-js-specs', function (callback) {
-  return del(paths.tmpSpecFiles, callback);
+  return del(['.tmp/specs.js', '.tmp/specs.js.map'], callback);
 });
 
 gulp.task('watch', function (done) {
-  gulp.watch(paths.srcTsFiles, ['test', 'tscompile']);
-  gulp.watch(paths.demoFiles, ['copyhtml']);
+  gulp.watch('src/**/*.ts', ['test', 'tscompile']);
+  gulp.watch('demo/**/*.html', ['copyhtml']);
   return done();
 });
 
